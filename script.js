765ad6a7d5dd8fd430e10cd42af27594ef0b9257
Nancy Chaudhry <nancych14@gmail.com>
2022-08-27 16:16:16 +0500

    Game working for 5 rounds and resets

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const buttons = document.querySelectorAll('button');
 const playerResult = document.querySelector('#player');
 const computerResult = document.querySelector('#computer');
 const roundResult = document.querySelector('#result');
+let h1 = document.querySelector('h1');
+let playerWins = document.querySelector('#player-wins');
+let computerWins = document.querySelector('#computer-wins');
+let round = 0;
+let computerTotal = 0;
+let playerTotal = 0;
+let gameWinner = '';
 
 // Gets random selection from computer
 function getComputerChoice() {
@@ -32,41 +39,53 @@ function playRound(playerSelection) {
         roundResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
         winner = 'computer';
     }
-    return { result, winner };
+    game(result, winner);
+}
+// Resets game for 5 more rounds
+function resetGame() {
+    round = 0;
+    computerTotal = 0;
+    playerTotal = 0;
+    gameWinner = '';
+    h1.textContent = `Let's Go Again!`;
+    roundResult.textContent = '';
+    playerWins.textContent = playerTotal;
+    computerWins.textContent = computerTotal;
+    buttons.forEach((button) => {
+        button.disabled = false;
+    });
 }
 
-//Gets player selection and computer selection
 // Runs 5 times
-function game() {
-    let computerTotal = 0;
-    let playerTotal = 0;
-    let gameWinner = '';
-
-    for (let i = 1; i <= 5; i++) {
-        let { result, winner } = playRound();
-        if (winner === 'player') {
-            ++playerTotal;
-
-        } else if (winner === 'computer') {
-            ++computerTotal;
+function game(result, winner) {
+    if (winner === 'player') {
+        ++playerTotal;
+        playerWins.textContent = playerTotal;
+    } else if (winner === 'computer') {
+        ++computerTotal;
+        computerWins.textContent = computerTotal;
+    }
+    if (round === 5) {
+        if (playerTotal === computerTotal) {
+            gameWinner = `It's a Draw`;
+        } else if (computerTotal > playerTotal) {
+            gameWinner = `Computer wins`;
+        } else if (playerTotal > computerTotal) {
+            gameWinner = `Player wins`;
         }
+        h1.innerHTML = `Game over.<br>${gameWinner} <button class="reset">Play Again?</button>`;
+        buttons.forEach((button) => {
+            button.disabled = true;
+        });
+        document.querySelector('.reset').addEventListener('click', resetGame);
     }
-
-    if (playerTotal === computerTotal) {
-        gameWinner = `It's a Draw`;
-    } else if (computerTotal > playerTotal) {
-        gameWinner = `Computer wins`;
-    } else if (playerTotal > computerTotal) {
-        gameWinner = `Player wins`;
-    }
-    console.log(`Game over. ${gameWinner}`);
 }
-
+// Listens for the players choice and increments rounds
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', () => {
+        round++;
+        h1.innerHTML = `Round ${round}`;
         playerSelection = button.id;
         playRound(playerSelection);
     });
 });
-
-// game();
